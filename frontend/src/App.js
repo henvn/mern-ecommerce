172578c550f2cd9bb3b0afaeb3ebcb6ae3bb1945
Henry <henryvn2205@gmail.com>
2021-12-12 23:13:28 +0700

    added latest product page showing product name  and image from hard data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
-    <h1>Welcome To Proshop</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product.id}`}>
+        <Card.Img src={product.image} variant="top" />
+        <Card.Body>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </Card.Text>
+
+          <Card.Text as="h3">${product.price}</Card.Text>
+        </Card.Body>
+      </a>
+    </Card>
+  );
+};
+
+export default Product;
