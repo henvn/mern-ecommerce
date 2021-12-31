89564dada81e3bf17a98b9914d2c2f88b3c5e5e4
Henry <henryvn2205@gmail.com>
2021-12-31 11:52:42 +1100

    fixed save shipping address issue

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,32 +1,34 @@
-import React, { useState } from "react"
-import { useNavigate, useLocation } from "react-router-dom"
+import React, { useState} from "react"
+import { useNavigate } from "react-router-dom"
 import { Form, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import FormContainer from "../components/FormContainer"
+import CheckoutSteps from "../components/CheckoutSteps"
 import { saveShippingAddress } from "../actions/cartActions"
 
 const ShippingScreen = () => {
-  const cart = useSelector(state => state.cart)
-  const {shippingAddress} = cart
-
+  
   const navigate = useNavigate()
-  const location = useLocation()
-  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const {shippingAddress} = cart 
 
   const [address, setAddress] = useState(shippingAddress.address)
   const [city, setCity] = useState(shippingAddress.city)
   const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
   const [country, setCountry] = useState(shippingAddress.country)
 
+  const dispatch = useDispatch()
+
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log("submitted")
-    dispatch(saveShippingAddress({address,city,postalCode,country}))
-    navigate("/payment")
+    dispatch(saveShippingAddress({address, city, postalCode, country}))
+    navigate('/payment')
   }
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2></CheckoutSteps>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
